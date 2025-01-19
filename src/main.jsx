5c178b0812465c6c3e5ff14fe018a15c72046568
siam114 <smsiamislam598@gmail.com>
2025-01-19 13:02:03 +0600

    added usesecure hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import router from './Routes/Route';
-import { HelmetProvider } from 'react-helmet-async';
-import AuthProvider from './context/AuthProvider';
-import { Toaster } from 'react-hot-toast';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Route";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./context/AuthProvider";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <HelmetProvider>
-     <AuthProvider>
-    <RouterProvider router={router} />
-    <Toaster position='top-right' reverseOrder={false} />
-    </AuthProvider>
-     </HelmetProvider>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <Toaster position="top-right" reverseOrder={false} />
+        </AuthProvider>
+      </HelmetProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
